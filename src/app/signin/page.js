775babb2f24c76dbdbe08019e539727e12851bb1
yunbeen <lillycho0810@naver.com>
2024-05-10 18:00:40 +0900

    signin페이지 page.js + css (아이콘 제외)

---
+++ src/app/signin/page.js
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import "./signin.css";
+
+export default function Signin() {
+    return(
+        <div>
+            <img src="/logo.png" className="logo"></img>
+            <div className="login-form">
+                <input type="text" placeholder="아이디" />
+                <input type="password" placeholder="비밀번호" />
+                <button type="submit">로그인</button>
+            </div>
+            <div className="to-signup">
+                <p><span>공감 다이어리</span>가 처음이신가요?</p>
+                <Link href="/signup">회원가입</Link>
+            </div>
+        </div>
+    )
+}
