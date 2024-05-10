f03f0df26fad30ba7f9d0d56ba84043078c8bc66
yunbeen <lillycho0810@naver.com>
2024-05-11 01:44:32 +0900

    signup페이지 백엔드 연동 시도 + signup-success페이지 생성

---
+++ src/app/signup-success/page.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+import "./signup-success.css";
+
+export default function SignupSuccess() {
+
+    return (
+        <div>
+
+        </div>
+    );
+}
--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,18 +1,69 @@
-"use client"
+"use client";
 
-import Link from 'next/link'
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 import "./signup.css";
 
-export default function Signin() {
-    return(
-        <form className="main-container">
+export default function Signup() {
+    const router = useRouter();
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const nickname = event.target.nickname.value;
+        const userId = event.target.id.value;
+        const password = event.target.password.value;
+        const passwordCheck = event.target['password-check'].value;
+        const email = event.target.email.value;
+
+        if (password !== passwordCheck) {
+            setErrorMessage("비밀번호와 확인 비밀번호가 일치하지 않습니다.");
+            return;
+        }
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ userId, email, nickname, password, confirmPassword: passwordCheck })
+        };
+
+        try {
+            const response = await fetch('http://43.201.47.70/users', options);
+            const resultText = await response.text();
+
+            if (response.ok) {
+                router.push('/signup-success');
+            } else {
+                switch (response.status) {
+                    case 400:
+                        setErrorMessage("비밀번호가 일치하지 않습니다.");
+                        break;
+                    case 409:
+                        setErrorMessage("이미 존재하는 ID입니다.");
+                        break;
+                    default:
+                        setErrorMessage("회원 가입에 실패했습니다.");
+                        break;
+                }
+            }
+        } catch (error) {
+            console.error('회원 가입 에러:', error);
+            setErrorMessage("서버 오류로 인해 회원 가입에 실패했습니다. 다시 시도해주세요.");
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="main-container">
             <div className="signup-form">
                 <input type="text" name="nickname" placeholder="닉네임" required />
                 <input type="text" name="id" placeholder="아이디" required />
                 <input type="password" name="password" placeholder="비밀번호" required />
                 <input type="password" name="password-check" placeholder="비밀번호 확인" required />
                 <input type="text" name="email" placeholder="이메일 주소" required />
-                <p className="retry-inst"></p>
+                <p className="retry-inst">{errorMessage}</p>
                 <button type="submit">회원가입</button>
             </div>
             <div className="to-signin">
@@ -20,5 +71,5 @@ export default function Signin() {
                 <Link href="/signin">로그인</Link>
             </div>
         </form>
-    )
+    );
 }
